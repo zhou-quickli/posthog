c4d7920f12e8f1528211a7312bf1de69522659aa
pauldambra <paul@posthog.com>
2025-12-07 12:43:37 +0000

    fix: push knowledge about the playlist down into the playlist component

--- frontend/src/scenes/session-recordings/player/sessionRecordingPlayerLogic.ts
+++ frontend/src/scenes/session-recordings/player/sessionRecordingPlayerLogic.ts
@@ -43,7 +43,6 @@ import { userLogic } from 'scenes/userLogic'
 import { AvailableFeature, ExporterFormat, RecordingSegment, SessionPlayerData, SessionPlayerState } from '~/types'
 
 import { ExportedSessionRecordingFileV2 } from '../file-playback/types'
-import type { sessionRecordingsPlaylistLogicType } from '../playlist/sessionRecordingsPlaylistLogicType'
 import { sessionRecordingEventUsageLogic } from '../sessionRecordingEventUsageLogic'
 import { playerCommentOverlayLogic } from './commenting/playerFrameCommentOverlayLogic'
 import { playerCommentOverlayLogicType } from './commenting/playerFrameCommentOverlayLogicType'
@@ -115,8 +114,6 @@ export interface SessionRecordingPlayerLogicProps extends SessionRecordingDataCo
     playerKey: string
     sessionRecordingData?: SessionPlayerData
     matchingEventsMatchType?: MatchingEventsMatchType
-    /** @deprecated Use onRecordingDeleted callback instead */
-    playlistLogic?: BuiltLogic<sessionRecordingsPlaylistLogicType>
     onRecordingDeleted?: () => void
     autoPlay?: boolean
     noInspector?: boolean
@@ -686,7 +683,6 @@ export const sessionRecordingPlayerLogic = kea<sessionRecordingPlayerLogicType>(
         // Prop references for use by other logics
         sessionRecordingId: [(_, p) => [p.sessionRecordingId], (sessionRecordingId) => sessionRecordingId],
         logicProps: [() => [(_, props) => props], (props): SessionRecordingPlayerLogicProps => props],
-        playlistLogic: [() => [(_, props) => props], (props) => props.playlistLogic],
 
         hasSnapshots: [
             (s) => [s.sessionPlayerData],
@@ -1650,9 +1646,6 @@ export const sessionRecordingPlayerLogic = kea<sessionRecordingPlayerLogicType>(
 
             if (props.onRecordingDeleted) {
                 props.onRecordingDeleted()
-            } else if (props.playlistLogic) {
-                props.playlistLogic.actions.loadAllRecordings()
-                props.playlistLogic.actions.setSelectedRecordingId(null)
             } else if (router.values.location.pathname.includes('/replay')) {
                 router.actions.push(urls.replay())
             }
